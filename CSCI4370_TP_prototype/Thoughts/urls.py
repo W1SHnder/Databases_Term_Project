94450210e7e28c752307821945af813756de8d74
Garison Zagorski <garisonzag88@gmail.com>
2024-07-28 23:44:14 -0400

    Frontend Implementation

--- CSCI4370_TP_prototype/Thoughts/urls.py
+++ CSCI4370_TP_prototype/Thoughts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.landing, name="landing"),
+    path("login", views.login, name="login"),
+    path("signup", views.signup, name="signup"),
+    path("main", views.main, name="main"),
+
 ]
--- CSCI4370_TP_prototype/Thoughts/views.py
+++ CSCI4370_TP_prototype/Thoughts/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
 
+def landing(request):
+    return render(request, 'landing.html', {})
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def login(request):
+    return render(request, 'login.html')
 
+def signup(request):
+    return render(request, 'signup.html', {})
+
+def main(request):
+    return render(request, 'main.html', {})
